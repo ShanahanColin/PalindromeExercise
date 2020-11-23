c86b3d15dcb3de4ee26628bc8cd58545a01b39c4
Colin Shanny <cshannyy@gmail.com>
2020-11-23 14:20:13 -0600

    test works

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            //reverse the word
+            var reversed = "";
+
+            for (int i = word.Length -1; i >= 0; i--)
+            {
+                reversed += word[i];
+            }
+
+            //compare original with reversed
+            if (reversed.ToLower() == word.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+                
+
+            //return whether they are the same or not
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
 using System;
+using PalindromeExercise;
 using Xunit;
+
+
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("RacecAR" , true)]
+        [InlineData("booK", false)]
+        public void Test1(string word , bool expected)
         {
+            //arrange
+            var tester = new WordSmith();
 
+            //act
+            bool actual = tester.IsAPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
